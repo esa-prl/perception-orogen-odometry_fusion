77dad0748fd6c27bac6d00f46349437d342d97bc
inigo.moreno <inigo.moreno@esa.int>
2020-11-20 19:05:18 +0000

    First version, only reads inputs.

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -2,18 +2,18 @@
 
 #include "Task.hpp"
 
+#include "base/samples/RigidBodyState.hpp"
+
+using namespace std;
 using namespace odometry_fusion;
+using namespace Eigen;
+using namespace base::samples;
 
-Task::Task(std::string const& name)
-    : TaskBase(name)
-{
-}
+IOFormat singleLine(StreamPrecision, DontAlignCols, ",\t", ";\t", "", "", "[", "]");
 
-Task::~Task()
-{
-}
+Task::Task(string const& name) : TaskBase(name), library(new OdometryFusion()) {}
 
-
+Task::~Task() { delete library; }
 
 /// The following lines are template definitions for the various state machine
 // hooks defined by Orocos::RTT. See Task.hpp for more detailed
@@ -21,29 +21,37 @@ Task::~Task()
 
 bool Task::configureHook()
 {
-    if (! TaskBase::configureHook())
-        return false;
+    if (!TaskBase::configureHook()) return false;
     return true;
 }
 bool Task::startHook()
 {
-    if (! TaskBase::startHook())
-        return false;
+    if (!TaskBase::startHook()) return false;
     return true;
 }
 void Task::updateHook()
 {
     TaskBase::updateHook();
+
+    RigidBodyState delta_pose;
+    if (_inertial_delta_pose_in.read(delta_pose) == RTT::NewData)
+    {
+        processInertialOdometryIn(delta_pose);
+    }
+
+    if (_visual_delta_pose_in.read(delta_pose) == RTT::NewData)
+    {
+        processVisualOdometryIn(delta_pose);
+    }
 }
-void Task::errorHook()
+void Task::processVisualOdometryIn(RigidBodyState delta_pose)
 {
-    TaskBase::errorHook();
+    cout << "[VISUAL]" << z.format(singleLine) << endl;
 }
-void Task::stopHook()
+void Task::processInertialOdometryIn(RigidBodyState delta_pose)
 {
-    TaskBase::stopHook();
+    cout << "[INERTIAL]" << delta_pose.position << endl;
 }
-void Task::cleanupHook()
-{
-    TaskBase::cleanupHook();
-}
+void Task::errorHook() { TaskBase::errorHook(); }
+void Task::stopHook() { TaskBase::stopHook(); }
+void Task::cleanupHook() { TaskBase::cleanupHook(); }
